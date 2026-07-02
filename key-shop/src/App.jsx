d098c1f97e9ad792f65b59c2e9481457ae476aff
Vinay Fagoida <vinay.fagodia250@gmail.com>
2026-07-02 23:05:52 +0530

    Toaster notification added using react hot tost

--- key-shop/src/App.jsx
+++ key-shop/src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,64 +7,103 @@ import Products from './pages/Products';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Cart from './pages/Cart';
-
+import toast, { Toaster } from 'react-hot-toast';
 
 import './App.css';
 
-function App(){
- // const [cartCount, setCartCount]=useState(0);
- const [cartItems, setCartItems] = useState([]);
-  function addToCart(product){
-    if(!product){
+function App() {
+  // const [cartCount, setCartCount]=useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  function addToCart(product) {
+    if (!product) {
       return;
     }
-    setCartItems((previousCartItems) =>{
-    const productAlreadyInCart= previousCartItems.find((item)=>item.id==product.id);
+    setCartItems((previousCartItems) => {
+      const productAlreadyInCart = previousCartItems.find((item) => item.id == product.id);
 
-      if(productAlreadyInCart){
+      if (productAlreadyInCart) {
         
-        return previousCartItems.map((item)=>{
-          return item.id==product.id ? {...item, quantity: item.quantity + 1 } : item;
+        return previousCartItems.map((item) => {
+          return item.id == product.id ? { ...item, quantity: item.quantity + 1 } : item;
         });
-      } 
-      return [...previousCartItems, {...product, quantity: 1}];
+      }
+
+      console.log(product);
+      return [...previousCartItems, { ...product, quantity: 1 }];
     })
+
+    product.quantity= product.quantity? product.quantity+1 : 1;
+    console.log(product);
+    if (product.stock > 3) {
+      toast.error(`${product.name} not available in stock!`);
+    }
+    else {
+      toast.success(`${product.name} worth ${product.price} added to the cart successfully`); // Template lateral method
+    }
+    // toast(product.name+' added to the cart successfully!');
+
   }
- 
-  function increaseQuantity(productId){
-    setCartItems((previousCartItems)=>{
-      return previousCartItems.map((item)=>{
-        return item.id == productId ? {...item, quantity:item.quantity+1} : item ;
+
+  function increaseQuantity(productId) {
+    setCartItems((previousCartItems) => {
+      return previousCartItems.map((item) => {
+        return item.id == productId ? { ...item, quantity: item.quantity + 1 } : item;
       });
     });
   }
 
-  function descreaseQuantity(productId){
-    setCartItems((previousCartItems)=>{
-     return previousCartItems.map((item)=>{
-        return item.id==productId ? {...item, quantity:item.quantity-1 } : item;
+  function descreaseQuantity(productId) {
+    setCartItems((previousCartItems) => {
+      return previousCartItems.map((item) => {
+        return item.id == productId ? { ...item, quantity: item.quantity - 1 } : item;
       }).filter((item) => item.quantity > 0)
     })
   }
 
-  function clearCart(){
+  function clearCart() {
     setCartItems([]);
   }
 
-  const cartCount = cartItems.reduce((total, item)=> total + item.quantity, 0);
-  
+  const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+
 
   return (
     <BrowserRouter>
       <div className="app">
         <Header cartCount={cartCount} />
+        <Toaster
+          reverseOrder={true}
+          position="top-center"
+          gutter={30}
+          
+          toastOptions={{
+            // Define default options
+            className: '',
+            duration: 5000,
+            removeDelay: 1000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              iconTheme: {
+                primary: 'green',
+                secondary: 'white',
+              },
+            },
+          }}
+      
+        />
         <main>
           <Routes>
             <Route path="/" element={<Home addToCart={addToCart} />} />
             <Route path="/products" element={<Products />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact-us" element={<Contact />} />
-            <Route path="/cart" element =  {<Cart cartItems={cartItems} increaseQuantity={increaseQuantity} decreaseQuantity={descreaseQuantity} clearCart={clearCart}/>} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} increaseQuantity={increaseQuantity} decreaseQuantity={descreaseQuantity} clearCart={clearCart} />} />
           </Routes>
         </main>
         <Footer />
--- key-shop/src/pages/Cart.jsx
+++ key-shop/src/pages/Cart.jsx
@@ -1,89 +1,89 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Cart({cartItems, increaseQuantity, decreaseQuantity, clearCart}){
+function Cart({ cartItems, increaseQuantity, decreaseQuantity, clearCart }) {
     console.log(cartItems);
-    const totalAmount= cartItems.reduce((total, item)=> total + item.price*item.quantity, 0);
-    const totalItems = cartItems.reduce((total,item)=>total + item.quantity, 0);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
-    if(cartItems.length === 0){
+    if (cartItems.length === 0) {
         return (
-        <section className="cartPage emptyCart">
-            <h1>Your Cart is Empty</h1>
-            <p>You have not added any key chain yet</p>
+            <section className="cartPage emptyCart">
+                <h1>Your Cart is Empty</h1>
+                <p>You have not added any key chain yet</p>
 
-            <Link to="/" class="continueShoppingBtn">Continue Shopping</Link>
-        </section>
+                <Link to="/" class="continueShoppingBtn">Continue Shopping</Link>
+            </section>
         )
     }
 
-   return (
-    <section className="cartPage">
-        <div className="cartHeading">
-            <h1>Your Shopping Cart</h1>
-            <button className="clearCartBtn" onClick={clearCart}>Clear Cart</button>
-        </div>
-        
-        <div className="cartLayout">
-            
-            <div class="cartItems">
-                {
-                    cartItems.map((item)=>(
-                        <div className="cartItem">
-                            <img src={item.image} alt={item.name} />
-                            <div className="cartItemDetails">
-                                <h3>{item.name}</h3>
-                                <p>₹{item.price} each</p>
+    return (
+        <section className="cartPage">
+            <div className="cartHeading">
+                <h1>Your Shopping Cart</h1>
+                <button className="clearCartBtn" onClick={clearCart}>Clear Cart</button>
+            </div>
 
-                                <div className="quantityBox">
-                                    <button onClick={()=>decreaseQuantity(item.id)}>
-                                        -
-                                    </button>
-                                    <span>{item.quantity}</span>
-                                    <button onClick={()=>increaseQuantity(item.id)}>
-                                        +
+            <div className="cartLayout">
+
+                <div class="cartItems">
+                    {
+                        cartItems.map((item) => (
+                            <div className="cartItem">
+                                <img src={item.image} alt={item.name} />
+                                <div className="cartItemDetails">
+                                    <h3>{item.name}</h3>
+                                    <p>₹{item.price} each</p>
+
+                                    <div className="quantityBox">
+                                        <button onClick={() => decreaseQuantity(item.id)}>
+                                            -
+                                        </button>
+                                        <span>{item.quantity}</span>
+                                        <button onClick={() => increaseQuantity(item.id)}>
+                                            +
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <div className="cartItemRight">
+                                    <strong>₹{item.price * item.quantity}</strong>
+                                    <button className="removeBtn">
+                                        Remove
                                     </button>
                                 </div>
-                            </div>
 
-                            <div className="cartItemRight">
-                             <strong>₹{item.price * item.quantity}</strong>
-                             <button className="removeBtn">
-                                Remove
-                             </button>
                             </div>
+                        ))
+                    }
 
-                        </div>
-                    ))
-                }
+                </div>
+
+                <div className="cartSummary">
+                    <h2>Order Summary</h2>
+
+                    <div className="summaryRow">
+                        <span>Total Items</span>
+                        <strong>
+                            {totalItems}
+                        </strong>
+                    </div>
+
+                    <div class="summaryRow totalRow">
+                        <span>Total Amount</span>
+                        <strong>₹{totalAmount}</strong>
+                    </div>
+
+                    <button class="checkOutBtn">Proceed to Checkout</button>
+                    <Link to="/" className="continueLink">Continue Shopping</Link>
+
+                </div>
 
             </div>
 
-             <div className="cartSummary">
-                <h2>Order Summary</h2>
 
-                <div className="summaryRow">
-                    <span>Total Items</span>
-                    <strong>
-                        {totalItems}
-                    </strong>
-                </div>
 
-                <div class="summaryRow totalRow">
-                    <span>Total Amount</span>
-                    <strong>₹{totalAmount}</strong>
-                </div>
-
-                <button class="checkOutBtn">Proceed to Checkout</button>
-                <Link to="/" className="continueLink">Continue Shopping</Link>
-
-        </div>
-      
-        </div>
-
-       
-
-    </section>
-   )
+        </section>
+    )
 }
 
 export default Cart;
