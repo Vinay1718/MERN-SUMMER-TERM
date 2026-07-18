feb93dbd773a0d53bcc0126a1ed78eb3e5a102f5
Vinay Fagoida <vinay.fagodia250@gmail.com>
2026-07-18 12:26:42 +0530

    APIs / CRUD operations on Products and Update the product by finding by ID

--- key-shop-apis/server.js
+++ key-shop-apis/server.js
@@ -1,25 +1,27 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import productRoutes from "./routes/productRoutes.js";
+const express= require("express");
+const Product = require("./models/Product.js");
+const productRoutes = require("./routes/productRoutes.js");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express(); // create nodejs / express app server
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const dotenv = require("dotenv");
 dotenv.config();
 
-const app = express();
-
-app.use(cors({ origin: "http://localhost:5173" }));
-app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected successfully!"))
+.catch((err) => console.log(err));
 
 app.use("/api/products", productRoutes);
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error.message);
-  });
+app.use("/", (req, res) => {
+    res.send("Welcome to the Key Shop API Backend!");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
